fe527996f454ae6b33283dc0635d52431a47318d
A.L <adlawren010@gmail.com>
2019-08-08 21:45:38 -0600

    Add sqlite, refactor

--- main.go
+++ main.go
@@ -1,84 +1,126 @@
 package main
 
 // TODO:
+// - Don't insert duplicate items
+// - Update config
+//   - Add params to limit the number of feed items retained in the database
+//     - And/or 'max retention time'
+//   - 'feed retrieval frequency'
+// - Add database entries to track feed retrieval times
+// - Display a 'loading' spinner/message when new feed items are being retrieved
+// - Add a manual 'refresh' button which prematurely retrieves the feeds (and updates the 'feed-last-retrieved' time in the db)
+// - Implement the feed retrieval function as a goroutine; run it without blocking the main app
 // - Add support for a http-param-based filter; i.e. <base url>?date=<something>&...
 //   - Ideally, you support parameters for each field in the item struct..
+// - Add support for a 'hidden' field in the feed config
+//   - Can hide feeds by default, by use a filter query ^ to see them
 // - Add CSS; Bootstrap
+// - Create/use hidden data dir
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/mmcdole/gofeed"
-	// "html"
 	"github.com/spf13/viper"
+	"gopkg.in/gormigrate.v1"
 	"html/template"
 	"log"
 	"net/http"
-	"net/url"
-	"sort"
 	"time"
 )
 
 type FeedItem struct {
-	Author      *gofeed.Person
+	gorm.Model
 	Title       string
 	Description string
 	Link        string
 	Published   *time.Time
-	Url         *url.URL
+	Url         string
+	AuthorName  string
+	AuthorEmail string
 }
 
-func convertToFeedItem(feedUrl string, gfi *gofeed.Item) *FeedItem {
-	feedItemUrl, _ := url.Parse(feedUrl)
+func convertToFeedItem(feedUrl string, goFeedItem *gofeed.Item) *FeedItem {
+	var goFeedAuthorName string
+	var goFeedAuthorEmail string
+	if goFeedItem.Author != nil {
+		goFeedAuthorName = goFeedItem.Author.Name
+		goFeedAuthorEmail = goFeedItem.Author.Email
+	}
+
 	return &FeedItem{
-		Author:      gfi.Author,
-		Title:       gfi.Title,
-		Description: gfi.Description,
-		Link:        gfi.Link,
-		Published:   gfi.PublishedParsed,
-		Url:         feedItemUrl,
+		Title:       goFeedItem.Title,
+		Description: goFeedItem.Description,
+		Link:        goFeedItem.Link,
+		Published:   goFeedItem.PublishedParsed,
+		Url:         feedUrl,
+		AuthorName:  goFeedAuthorName,
+		AuthorEmail: goFeedAuthorEmail,
 	}
-}
-
-func convertToFeedItems(feedUrl string, goFeedItems []*gofeed.Item) []*FeedItem {
-	var feedItems []*FeedItem
-	for _, gfi := range goFeedItems {
-		feedItems = append(feedItems, convertToFeedItem(feedUrl, gfi))
-	}
-
-	return feedItems
-}
-
-type FeedItemSlice []*FeedItem
-
-func (fis FeedItemSlice) Len() int {
-	return len(fis)
-}
-
-func (fis FeedItemSlice) Less(i, j int) bool {
-	iTime := fis[i].Published
-	jTime := fis[j].Published
-
-	return iTime.After(*jTime)
-}
-
-func (fis FeedItemSlice) Swap(i, j int) {
-	fis[i], fis[j] = fis[j], fis[i]
 }
 
 type IndexTemplateParams struct {
-	Title    string
-	ItemList FeedItemSlice
+	Title     string
+	FeedItems []*FeedItem
+}
+
+func migrateDatabase(db *gorm.DB) error {
+	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
+		{
+			ID: "201908072046",
+			Migrate: func(tx *gorm.DB) error {
+				if err := tx.AutoMigrate(&FeedItem{}).Error; err != nil {
+					return err
+				}
+
+				return nil
+			},
+			Rollback: func(tx *gorm.DB) error {
+				if err := tx.DropTable("feed_items").Error; err != nil {
+					return err
+				}
+
+				return nil
+			},
+		},
+	})
+
+	return m.Migrate()
+}
+
+func loadConfig(configPath, configName string) error {
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(configPath)
+	return viper.ReadInConfig()
+}
+
+// TODO: see if there's a way to avoid the redundancy - the feedUrl is in the feedConfigMap too
+func processGoFeedItem(db *gorm.DB, goFeedItem *gofeed.Item, feedUrl string, feedConfigMap map[string]interface{}) {
+	fi := convertToFeedItem(feedUrl, goFeedItem)
+
+	db.Create(fi)
 }
 
 func main() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Error: failed to read config file")
-		return
+	if err := loadConfig(".", "config"); err != nil {
+		log.Fatal(err)
 	}
 
-	var allFeedItems FeedItemSlice
+	// TODO: add a config param to enable logging
+	// db.LogMode(true)
+
+	db, err := gorm.Open("sqlite3", fmt.Sprintf("%v.sqlite3", viper.GetString("db_name")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	if err := migrateDatabase(db); err != nil {
+		log.Fatal(err)
+	}
+
 	fp := gofeed.NewParser()
 
 	feedConfigs := viper.Get("feeds").([]interface{})
@@ -109,26 +151,27 @@ func main() {
 				feedItemLimit = len(nextFeed.Items)
 			}
 
-			allFeedItems = append(
-				allFeedItems,
-				convertToFeedItems(
-					feedUrl,
-					nextFeed.Items[:feedItemLimit])...)
+			for _, goFeedItem := range nextFeed.Items[:feedItemLimit] {
+				processGoFeedItem(db, goFeedItem, feedUrl, feedConfigMap)
+			}
 		}
 	}
 
-	sort.Sort(allFeedItems)
+	dbOrderedByPublished := db.Order("published DESC", true)
+
+	var feedItems []*FeedItem
+	dbOrderedByPublished.Find(&feedItems, &FeedItem{})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// TODO: parse url params, filter items
 		//fmt.Println(r.URL.Query())
 
 		if t, err := template.ParseFiles("index.html"); err != nil {
-			fmt.Println("Error: failed to parse template")
+			log.Fatal(err)
 		} else {
 			t.Execute(w, &IndexTemplateParams{
-				Title:    viper.GetString("homepage_title"),
-				ItemList: allFeedItems,
+				Title:     viper.GetString("homepage_title"),
+				FeedItems: feedItems,
 			})
 		}
 	})
