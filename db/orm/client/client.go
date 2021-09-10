0aa4bc2ca4fe92b558432e609d7b40c5be8ed6c0
A.L <adlawren010@gmail.com>
2021-09-09 20:02:43 -0600

    Add docstrings

--- db/orm/client/client.go
+++ db/orm/client/client.go
@@ -25,11 +25,14 @@ type client struct {
 	db *sql.DB
 }
 
+// All fetches the models from the appropriate table and assigns the result to the given interface
 func (c *client) All(ptr interface{}) error {
-	// All just calls FindAll without using any clauses, but "All(ptr)" is faster to type than "FindAll(ptr)"
+	// All just calls FindAll without using any clauses
+	// It exists b/c "All(ptr)" is faster to type than "FindAll(ptr)"
 	return c.FindAll(ptr)
 }
 
+// Find fetches the first model from the appropriate table and assigns the result to the given interface, subject to the given query clauses
 func (c *client) Find(ptr interface{}, clauses ...*query.Clause) error {
 	query, err := query.SelectOne(ptr, clauses...)
 	if err != nil {
@@ -39,6 +42,7 @@ func (c *client) Find(ptr interface{}, clauses ...*query.Clause) error {
 	return errors.Wrap(query.Exec(c.db), "failed to execute query")
 }
 
+// FindAll fetches the models from the appropriate table and assigns the result to the given interface, subject to the given query clauses
 func (c *client) FindAll(ptr interface{}, clauses ...*query.Clause) error {
 	query, err := query.Select(ptr, clauses...)
 	if err != nil {
@@ -48,6 +52,7 @@ func (c *client) FindAll(ptr interface{}, clauses ...*query.Clause) error {
 	return errors.Wrap(query.Exec(c.db), "failed to execute query")
 }
 
+// Save inserts the model into the appropriate table if it has an unspecified ID, and updates it otherwise
 func (c *client) Save(ptr interface{}) error {
 	query, err := query.Upsert(ptr)
 	if err != nil {
