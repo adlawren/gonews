1abf9e90772cc49ffd53aae21db3147550428b3f
A.L <adlawren010@gmail.com>
2021-09-09 20:49:24 -0600

    Replace github.com/pkg/errors with stdlib errors

--- auth/auth.go
+++ auth/auth.go
@@ -1,11 +1,12 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
 	"gonews/db"
 	"gonews/db/orm/query"
 	"gonews/user"
 
-	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,20 +16,27 @@ func IsValid(username, password string, db db.DB) (bool, error) {
 	if errors.Is(err, query.ErrModelNotFound) {
 		return false, nil
 	} else if err != nil {
-		return false, errors.Wrap(err, "failed to get matching user")
+		return false, fmt.Errorf("failed to get matching user: %w", err)
 	}
 
 	err = bcrypt.CompareHashAndPassword(
 		[]byte(user.PasswordHash),
 		[]byte(password))
 	if err == bcrypt.ErrMismatchedHashAndPassword {
-		return false, errors.Wrap(err, "password invalid")
+		return false, fmt.Errorf("password invalid: %w", err)
+	}
+	if err != nil {
+		return false, fmt.Errorf("failed to compare hash and password: %w", err)
 	}
 
-	return err == nil, errors.Wrap(err, "failed to compare hash and password")
+	return true, nil
 }
 
 func Hash(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(hash), errors.Wrap(err, "failed to generate password hash")
+	if err != nil {
+		return "", fmt.Errorf("failed to generate password hash: %w", err)
+	}
+
+	return string(hash), nil
 }
