b6e4db9b57e190a16a332a62bf55e7be88c0984c
A.L <adlawren010@gmail.com>
2021-09-19 16:21:05 -0600

    Rename str field to text

--- db/orm/query/clause/clause.go
+++ db/orm/query/clause/clause.go
@@ -2,12 +2,12 @@ package clause
 
 // Clause represents an SQL clause in a SQL query
 type Clause struct {
-	str  string
+	text string
 	args []interface{}
 }
 
 func (c *Clause) Text() string {
-	return c.str
+	return c.text
 }
 
 func (c *Clause) Args() []interface{} {
@@ -17,7 +17,7 @@ func (c *Clause) Args() []interface{} {
 // New creates a Clause from the given arguments
 func New(clause string, args ...interface{}) *Clause {
 	return &Clause{
-		str:  clause,
+		text: clause,
 		args: args,
 	}
 }
